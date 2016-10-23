502f7eccb2bd2fa39657e2445fe2700fc7f1e56d
Tamás Szelei <hello.sztomi@gmail.com>
2016-10-23 11:23:42 +0200

    gui: added resources

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QStyleFactory
 from ppedit.gui.windows import MainWindow
+from resources import qInitResources, qCleanupResources
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create("Fusion"))
+    qInitResources()
     w = MainWindow()
     w.show()
+    qCleanupResources()
     sys.exit(app.exec_())
--- ppedit/gui/windows.py
+++ ppedit/gui/windows.py
@@ -1,9 +1,19 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import pyqtSlot
+from qutepart import Qutepart
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         loadUi('res/mainwindow.ui', self)
+        self.statusBar()
+        self.editor = Qutepart(self)
+        self.setCentralWidget(self.editor)
+
+    @pyqtSlot()
+    def on_actionOpen_triggered(self):
+        QMessageBox.information(self, 'triggered!', 'actionOpen')
+
 
