cdd12ded945b7e6493eca6132bd66520740af1ba
sztomi <szelei.t@gmail.com>
2016-10-23 23:07:17 +0200

    pp: refactored text processing into its own class

--- ppedit/preprocessor.py
+++ ppedit/preprocessor.py
@@ -2,6 +2,9 @@ from abc import ABCMeta, abstractmethod
 from typing import List
 from subprocess import run, PIPE
 from os import remove
+from io import StringIO
+
+from ppedit.tools.bases import TextProcessor
 
 
 class Preprocessor(object, metaclass=ABCMeta):
@@ -11,24 +14,15 @@ class Preprocessor(object, metaclass=ABCMeta):
 
 
 class ClangPreprocessor(Preprocessor):
-    def get_preprocessed_contents(self, filename: str, input_file: str, sketch: str, flags: List[str]) -> str:
-        sketch_name = '{}.sketch.h'.format(filename)
-        marker = 'PPEDIT_MARK_SKETCH'
-        with open(sketch_name, 'w') as sketch_file:
-            print(input_file, file=sketch_file)
-            print(marker, file=sketch_file)
-            print(sketch, file=sketch_file)
+    marker = 'PPEDIT_MARK_SKETCH'
 
-        complete_flags = ['-E', *flags, '-x', 'c++', '-D{}={}'.format(marker, marker), sketch_name]
-        command = ['clang++', *complete_flags]
-        clang = run(command, stdout=PIPE)
-        preprocessed_name = '{}.preprocessed.h'.format(sketch_name)
-        with open(preprocessed_name, 'w') as f:
-            f.write(clang.stdout.decode('utf-8'))
-        command = ['clang-format', preprocessed_name]
-        clang_format = run(command, stdout=PIPE)
-        lines = clang_format.stdout.decode('utf-8')
-        marker_idx = lines.find(marker)
-        remove(sketch_name)
-        remove(preprocessed_name)
-        return ''.join(lines[marker_idx + len(marker):])
+    def get_preprocessed_contents(self, filename: str, input_text: str, sketch: str, flags: List[str]) -> str:
+        text = '{input}\n{marker}\n{sketch}'.format(input=input_text, marker=self.marker, sketch=sketch)
+        pp = TextProcessor('clang++')
+        assembled_flags = '-E {flags} -x c++ -D{marker}={marker}'.format(flags=' '.join(flags), marker=self.marker)
+        assembled_flags += ' {input_file}'
+        processed = pp.process_text(text, assembled_flags)
+        clang_format = TextProcessor('clang-format')
+        lines = clang_format.process_text(processed, '{input_file}')
+        marker_idx = lines.find(self.marker)
+        return ''.join(lines[marker_idx + len(self.marker) + 1:])
